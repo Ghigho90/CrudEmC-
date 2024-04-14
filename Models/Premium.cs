4e26c8cadc4e5ac87b2a012fc06a63284b72d819
Ghigho90 <ghigho33@gmail.com>
2024-04-14 20:38:09 -0300

    Primeiro Commit

---
+++ Models/Premium.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace GhighoTest.Models
+{
+    public class Premium
+    {
+        [Key]
+        [DisplayName("Id")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Informe o titulo do premium")]
+        [MaxLength(80, ErrorMessage = "O titulo do premium deve ter no máximo 80 caracteres")]
+        [MinLength(5, ErrorMessage = "O titulo do premium deve ter no mínimo 5 caracteres")]
+        [DisplayName("Titulo")]
+        public string Title { get; set; }
+
+        [DataType(DataType.DateTime)]
+        //[GreaterThanToday]
+        [DisplayName("Data de Início")]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.DateTime)]
+        //[GreaterThanToday]
+        [DisplayName("Data de Término")]
+        public DateTime EndDate { get; set; }
+
+        [Required(ErrorMessage = "Informe o aluno")]
+        [DisplayName("Aluno")]
+        public Guid StudentId { get; set; }
+        public Student? Student { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace GhighoTest.Models
+{
+    public class Student
+    {
+        [Key]
+        [DisplayName("Id")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage ="Informe o nome")]
+        [MaxLength(80, ErrorMessage = "O nome deve ter no máximo 80 caracteres")]
+        [MinLength(5, ErrorMessage = "O nome deve ter no mínimo 5 caracteres")]
+        [DisplayName("Nome Completo")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Informe o e-mail")]
+        [EmailAddress(ErrorMessage ="E-mail inválido")]
+        [DisplayName("E-mail")]
+        public string Email { get; set; }
+
+        public List<Premium> Premiums { get; set; } = new();
+    }
+}
